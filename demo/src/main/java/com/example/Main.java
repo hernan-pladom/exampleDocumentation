4c3f6141131e1e35918fe5d1123dbe5584f8ea58
hernan-pladom <hernan.pladom@usal.es>
2024-12-10 19:06:42 +0100

    first commit

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+public class Main {
+    /**
+	 * 
+	 * Main method of the program
+	 * 
+     * @param args The command line arguments
+	 * 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+		Gson custom = new Gson();
+    }
+}
